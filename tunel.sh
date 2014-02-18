17625ecea4296fe3e8b2e23ce6e78ca9e00c4fa9
pdostal <git@pdostal.cz>
2014-02-18 15:02:32 +0100

    Change PostgreSQL commands from Postgresql93.app to homebrew PostgreSQL

--- tunel.sh
+++ tunel.sh
@@ -2,7 +2,7 @@
 echo "Starting: " `date`
 for ((;;))
 do
-	ssh -o ExitOnForwardFailure=yes -o TCPKeepAlive=yes -o ServerAliveCountMax=2 -o ServerAliveInterval=10 -N -L 1465:localhost:465 -L 1080:localhost:1080 -L 3000:localhost:3000 -L 4000:localhost:4000 hom
+	ssh -o ExitOnForwardFailure=yes -o TCPKeepAlive=yes -o ServerAliveCountMax=2 -o ServerAliveInterval=10 -N -L 1465:localhost:465 -L hom
 	echo "Restarting: " `date`
 	sleep 1
 done
